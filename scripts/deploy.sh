afcd27a6432fe62123b21eaea9ff44ff712526c9
Toemmsen96 <147745097+Toemmsen96@users.noreply.github.com>
2026-03-23 20:15:59 +0100

    🚀 deploy script improvement

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-IP=$(ip route get 1 | awk '{print $7; exit}' | sed 's/\.[0-9]*$/.104/')
+OS="$(uname)"
 
-rsync -avz --delete "$1" "pi@$IP:netcore/$2/"
+if [[ "$OS" == "Linux" ]]; then
+    IP=$(ip route get 1 | awk '{print $7; exit}' | sed 's/\.[0-9]*$/.104/')
+    rsync -avz --delete "$1" "pi@$IP:netcore/$2/"
+elif [[ "$OS" == "Darwin" ]]; then
+    rsync -avz --delete "$(TargetDir)" "csa:netcore/$(ProjectName)"
+else
+    echo "Unsupported OS: $OS"
+    exit 1
+fi
