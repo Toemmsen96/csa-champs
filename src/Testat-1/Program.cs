774598187619ca28824544ac16077b01edf67736
Toemmsen96 <147745097+Toemmsen96@users.noreply.github.com>
2026-04-13 16:18:33 +0200

    Color stuff

--- src/Testat-1/Program.cs
+++ src/Testat-1/Program.cs
@@ -21,7 +21,6 @@ class Program
 #if DEBUG
         Debugger.WaitForDebugger();
 #endif
-        new Task(() => Zumo.Instance.RTTTL.PlaySong(RtttlSong.MissionImpossible)).Start();
         Program program = new();
         await program.RunAsync();
         //Zumo.Instance.Drive.Turn(360,70,70);
@@ -29,7 +28,9 @@ class Program
 
     private async Task RunAsync()
     {
+        Zumo.Instance.RTTTL.PlayRtttlOnce("Sandstor:d=16,o=5,b=112:d6,d6,d6,d6,8d6,d6,d6,d6,d6,d6,d6,8d6,g6,g6,g6,g6,g6,g6,g6,g6,f6,f6,f6,f6,f6,f6,8f6,c6,c6,d6,d6,d6,d6,8d6,d6,d6,d6,d6,d6,d6,d6,d6,f6,f6,d6,d6,d6,d6,d6");
         Zumo.Instance.Lidar.SetPower(true);
+        Calibrate();
         await Task.Delay(1000);
 
         try
@@ -74,10 +75,52 @@ class Program
         throw new InvalidOperationException("No valid move found on the current layer.");
     }
 
+     private void Calibrate()
+    {
+        Console.WriteLine("Starting calibration...");
+        Console.WriteLine("Please place the robot on white surface and press the CM4 button or Enter in the console.");
+        while (!Zumo.Instance.Cm4Button.Pressed)
+        {
+            if (Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Enter)
+            {
+                break;
+            }
+            Thread.Sleep(100);
+        }
+        Zumo.Instance.ColorSensor.Calibrate(ColorSensor.CalibrationColor.White);
+        Console.WriteLine("White calibrated. Please place the robot on black surface and press the CM4 button or Enter in the console.");
+        while (!Zumo.Instance.Cm4Button.Pressed)
+        {
+            if (Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Enter)
+            {
+                break;
+            }
+            Thread.Sleep(100);
+        }
+        Zumo.Instance.ColorSensor.Calibrate(ColorSensor.CalibrationColor.Black);
+        Console.WriteLine("Calibration completed.");
+    }
+
     private bool IsAllowedToLeave()
     {
         Node node = Map.GetCurrentNode();
-        return node.NodeLevel == 0 || false;
+        string rgb = Zumo.Instance.ColorSensor.ReadColorRGB();
+        Console.WriteLine($"Current Node Level: {node.NodeLevel}, Detected Color RGB: {rgb}");
+        int red = Convert.ToInt32(rgb.Substring(1, 2), 16);
+        int green = Convert.ToInt32(rgb.Substring(3, 2), 16);
+        int blue = Convert.ToInt32(rgb.Substring(5, 2), 16);
+        if (red >= 200 && green < 100 && blue < 100 && node.NodeLevel == 0)
+        {
+            return true;
+        }
+        else if (red < 100 && green >= 200 && blue < 100 && node.NodeLevel == 1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
     private bool TryGetExit(out Direction? moveDirection)
